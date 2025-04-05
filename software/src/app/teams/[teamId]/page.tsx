da065908d900041fe3b58b9acfdeaa4fe46ba8f4
Pulasthi Lenaduwa <plen276@aucklanduni.ac.nz>
2025-04-06 03:54:05 +1200

    💄 style [Software]: Fixed tab issue

--- software/src/app/teams/[teamId]/page.tsx
+++ software/src/app/teams/[teamId]/page.tsx
@@ -3,12 +3,12 @@
 import { useParams } from "next/navigation"
 
 export default function TeamPage() {
-	const { teamId } = useParams()
+  const { teamId } = useParams()
 
-	return (
-		<div>
-			<h1 className="text-xl font-bold">Team: {decodeURIComponent(teamId as string)}</h1>
-			<p>Details about {decodeURIComponent(teamId as string)} go here...</p>
-		</div>
-	)
+  return (
+    <div>
+      <h1 className="text-xl font-bold">Team: {decodeURIComponent(teamId as string)}</h1>
+      <p>Details about {decodeURIComponent(teamId as string)} go here...</p>
+    </div>
+  )
 }
