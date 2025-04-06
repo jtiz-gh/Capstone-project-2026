554333735afff7554c18e0505acf8424c26ac050
Chris Valenzuela <55444457+chrisv09@users.noreply.github.com>
2025-04-06 23:07:26 +1200

    ✨ feat [Software/HomePage]: UI clean up
    * ✨feat [Software/Component]: Added buttons
    * ✨ feat [Software/Component]: Centered a header
    * ✨ feat [Software/Layout]: Removed Navbar from Layout
    * ♻️ refactor [Software/Navbar]: Removed unneeded Navbar import
    * ✨ feat [Software/Homepage]: Removed unneeded button
    * ✨ feat [Software/Homepage]: Added icons for teams and competitions
    * 🐛 fix [Software/Sensors]: Buttons are the correct size now
    * ✨ feat [Software/Design]: Made heading bigger and removed footer

--- software/src/app/layout.tsx
+++ software/src/app/layout.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -28,7 +27,6 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				<Navbar />
 				{children}
 			</body>
 		</html>
