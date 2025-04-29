f1d19f1966e5021df33a41ca8510d1b92591be09
Chris Valenzuela <55444457+chrisv09@users.noreply.github.com>
2025-04-29 18:41:19 +1200

    ✨ feat [Software/E2E Testing]: Add cypress for e2e testing (#46)
    * package: Installed Cypress
    * scripts: Added Cypress to dev scripts
    * config: Added config file for Cypress
    * config: Created simple e2e test for cypress
    * package: Updated `package.json`
    ---------
    Co-authored-by: Pulasthi Lenaduwa <plen276@aucklanduni.ac.nz>
    * bug: fixed `package.json` and `package-lock.json`

---
+++ software/cypress/support/e2e.tsx
@@ -0,0 +1,5 @@
+beforeEach(() => {
+    cy.log("Before each test, we clear the local storage and session storage to ensure a clean state.")
+    cy.clearLocalStorage()
+    cy.clearAllSessionStorage()
+})
