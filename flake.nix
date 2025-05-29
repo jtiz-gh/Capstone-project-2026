873457afe8417f3bda31a5c128f7929f676ff71a
Excigma <30280397+Excigma@users.noreply.github.com>
2025-05-29 20:57:33 +1200

    :ambulance: hotfix [Software]: Fix Prisma binaries on NixOS (#158)
    * :ambulance: hotfix [Software]: Fix Prisma binaries on NixOS
    * fix: Prisma openssl missing warning
    prisma:warn Prisma failed to detect the libssl/openssl version to use, and may not work as expected. Defaulting to "openssl-1.1.x".

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   description = "Development environment for Raspberry Pi Pico";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.prisma-utils.url = "github:VanCoding/nix-prisma-utils";
 
   outputs =
-    { self, nixpkgs }:
+    { self, prisma-utils, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -21,6 +22,15 @@
           sha256 = "sha256-pQe0V6iAPqZfpauZZnva+WjYC/GW83RQS8uoBxjgcHg=";
         };
       };
+      prisma =
+        (prisma-utils.lib.prisma-factory {
+          inherit pkgs;
+          prisma-fmt-hash = "sha256-MQnSmx4+S6lQWyn/l2CccbJZG0uHzb4gJV5luAnDl+A=";
+          query-engine-hash = "sha256-ttsqP6XJuo/iIDFX2VqyOaRKsvE9qDDk8Q7Y0aDm71s=";
+          libquery-engine-hash = "sha256-oOuR8XtO3I7NDUtx/JXjzHjBxDEFO8jv3x5CgccMzjc=";
+          schema-engine-hash = "sha256-3Z76iqOAR5ytdfOkq5XQofnUveXqoqibNjaChGKisiM=";
+        }).fromNpmLock
+          ./software/package-lock.json; # <--- path to our package-lock.json file that contains the version of prisma-engines
     in
     {
       devShells.${system}.default = pkgs.mkShell rec {
@@ -30,6 +40,8 @@
           pnpm
           docker
           docker-compose
+
+          openssl
 
           # FIRMWARE
           # Required for MicroPython development
@@ -63,6 +75,8 @@
           # hidapi
           # zstd
         ];
+
+        env = prisma.env;
 
         shellHook = ''
           export PICO_SDK_PATH="${local-pico-sdk}/lib/pico-sdk"
